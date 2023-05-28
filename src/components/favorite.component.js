e6296a28f9b50a78381f3b6aa7b914dbec27520a
SoraI0 <romamogir@gmail.com>
2023-05-29 01:38:34 +0300

    first commit

---
+++ src/components/favorite.component.js
@@ -0,0 +1,48 @@
+import { Component } from "../core/component";
+import { apiService } from "../services/api.services";
+import { renderPost } from "../tamplates/post.template";
+
+export class FavoriteComponent extends Component {
+	constructor(id, { loader }) {
+		super(id)
+		this.loader = loader
+	}
+
+	init() {
+		this.$el.addEventListener('click', linkClickHandler.bind(this))
+	}
+
+	onShow() {
+		const favorites = JSON.parse(localStorage.getItem('favorites'))
+		const html = renderList(favorites)
+		this.$el.insertAdjacentHTML('afterbegin', html)
+	}
+	onHide() {
+		this.$el.innerHTML = ''
+	}
+}
+
+async function linkClickHandler(event) {
+	event.preventDefault()
+
+	if (event.target.classList.contains('js-link')) {
+		const postId = event.target.textContent
+		this.$el.innerHTML = ''
+		this.loader.show()
+		const post = await apiService.fetchPostById(postId)
+		this.loader.hide()
+		this.$el.insertAdjacentHTML('afterbegin', renderPost(post, { withButton: false }))
+	}
+}
+
+function renderList(list = []) {
+	if (list.length) {
+		return `
+			<ul>
+				${list.map(i => `<li><a href="#" class="js-link">${i}</a></li>`).join(' ')}
+			</ul>
+		`
+	}
+
+	return `<p class='center'>Поки що ви нічого не добавили</p>`
+}
